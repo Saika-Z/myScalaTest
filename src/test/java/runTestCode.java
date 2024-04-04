4aa5afb22eba792a5e1ee9e4c0fcb5c59610f030
Saika-Z <vincentzhao0814@gmail.com>
2024-04-04 10:28:20 +0800

    update

--- src/test/java/runTestCode.java
+++ src/test/java/runTestCode.java
@@ -8,29 +8,35 @@ import java.util.*;
  * ClassName: runTestCode
  * Description: test runCode
  * Author: Zhao Wenqing
- * Date:  2024/1/21 09:09
+ * Date: 2024/1/21 09:09
  *************************************************************************************/
 public class runTestCode {
 
     public static void main(String[] args) {
-        /*TreeNode treeNode = TreeNode.mkTree("1,2,3,4,5");
-        ArrayList<Integer> integers1 =new TreeNode().beforeTraverse(treeNode);
-        ArrayList<Integer> integers2 =new TreeNode().afterTraverse(treeNode);
-        ArrayList<Integer> integers3 =new TreeNode().sequenceTraverse(treeNode);
-        ListNode listNode = ListNode.createListNode(new int[]{1, 2, 3, 4, 5});
-        ListNode.printListNode(listNode);
-        ListNode listNode1 = new testSolution().removeNthFromEnd(listNode, 2);
-        ListNode.printListNode(listNode1);
-        System.out.printf("前序: %s\n",integers1.toString());
-        System.out.printf("后序: %s\n",integers2.toString());
-        System.out.printf("层序: %s\n",integers3.toString());*/
-        ListNode l1 = ListNode.createListNode(new int[]{0, 1, 13, 6, 9, 5});
-        ListNode l2 = ListNode.createListNode(new int[]{1000000,1000001,1000002});
-        ListNode listNode = new testSolution().mergeInBetween(l1, 3, 4, l2);
-        ListNode.printListNode(listNode);
-        //new testSolution().numSubarraysWithSum(new int[]{0,0,0,0,0},0);
-
-
+        /*
+         * TreeNode treeNode = TreeNode.mkTree("1,2,3,4,5");
+         * ArrayList<Integer> integers1 =new TreeNode().beforeTraverse(treeNode);
+         * ArrayList<Integer> integers2 =new TreeNode().afterTraverse(treeNode);
+         * ArrayList<Integer> integers3 =new TreeNode().sequenceTraverse(treeNode);
+         * ListNode listNode = ListNode.createListNode(new int[]{1, 2, 3, 4, 5});
+         * ListNode.printListNode(listNode);
+         * ListNode listNode1 = new testSolution().removeNthFromEnd(listNode, 2);
+         * ListNode.printListNode(listNode1);
+         * System.out.printf("前序: %s\n",integers1.toString());
+         * System.out.printf("后序: %s\n",integers2.toString());
+         * System.out.printf("层序: %s\n",integers3.toString());
+         */
+        // ListNode l1 = ListNode.createListNode(new int[]{0, 1, 13, 6, 9, 5});
+        // ListNode l2 = ListNode.createListNode(new int[]{1000000,1000001,1000002});
+        // ListNode listNode = new testSolution().mergeInBetween(l1, 3, 4, l2);
+        // ListNode.printListNode(listNode);
+        // new testSolution().numSubarraysWithSum(new int[]{0,0,0,0,0},0);
+        char[][] arc= new char [][]{
+            {'A','B','C','E'},
+            {'S','F','C','S'},
+            {'A','D','E','E'}
+        };
+       boolean s = new testSolution().exist(arc,"ABCCED");
     }
 
 }
--- src/test/java/testSolution.java
+++ src/test/java/testSolution.java
@@ -20,20 +20,112 @@ public class testSolution {
             a -= 1;
             b -= 1;
             dummy = dummy.next;
-            if (a == 0) workList1 = dummy;
+            if (a == 0)
+                workList1 = dummy;
             if (b == 0) {
                 workList2 = dummy;
                 break;
             }
 
         }
-        if (workList1 == null) workList1 = list1;
+        if (workList1 == null)
+            workList1 = list1;
         workList1.next = list2;
         while (list2.next != null) {
             list2 = list2.next;
         }
         list2.next = workList2.next;
         return list1;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int n = board.length, m = board[0].length;
+        boolean[][] visited = new boolean[n][m];
+        char[] wordChar = word.toCharArray();
+        if (wordChar.length > n * m)
+            return false;
+        int counts[] = new int[256];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                counts[board[i][j]]++;
+            }
+        }
+        int len = wordChar.length;
+        for(int i=0; i<len/2; i++)
+        {
+            if(counts[wordChar[i]]>counts[wordChar[len-1-i]]){
+                for(int j=0; j<len/2; j++)
+                {
+                    char temp = wordChar[j];
+                    wordChar[j]=wordChar[len-1-j];
+                    wordChar[len-1-j]=temp;
+                }
+                break;
+            }
+        }
+        for (char c : wordChar) {
+            if (--counts[c] < 0)
+                return false;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visit(board, wordChar, 0, i, j, n, m, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean visit(char[][] board, char[] word, int start, int x, int y,
+            int n, int m, boolean[][] visited) {
+        if (start == word.length)
+            return true;
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y])
+            return false;
+        if (word[start] != board[x][y])
+            return false;
+        visited[x][y] = true;
+        boolean found = visit(board, word, start + 1, x + 1, y, n, m, visited)
+                || visit(board, word, start + 1, x - 1, y, n, m, visited)
+                || visit(board, word, start + 1, x, y + 1, n, m, visited)
+                || visit(board, word, start + 1, x, y - 1, n, m, visited);
+        visited[x][y] = false;
+        return found;
+    }
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int index = 0, count = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+            while (index <= i && mp.size() > k) {
+                if (mp.get(nums[i]) == 1) {
+                    mp.remove(nums[i]);
+                } else {
+                    mp.put(nums[i], mp.get(nums[i]) - 1);
+                }
+                index += 1;
+            }
+            System.out.printf("index:%d|i:%d\n", index, i);
+            count += (i - index + 1);
+        }
+        System.out.printf("result : %d\n", count);
+        return count;
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] arr1 = new int[200];
+        int[] arr2 = new int[200];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (arr1[s.charAt(i)] != arr2[t.charAt(i)])
+                return false;
+            arr1[s.charAt(i)] += 1;
+            arr2[t.charAt(i)] += 1;
+        }
+        return true;
     }
 
     public String mySubstring(Integer[] arr, String s) {
@@ -77,40 +169,40 @@ public class testSolution {
         // Your runtime beats 10.79 % of java submissions
         // Your memory usage beats 99.35 % of java submissions (46.6 MB)
         /*
-          if (head.next == null || head.next.next == null)
-                      return;
-                  ListNode slow = head;
-                  ListNode fast = head.next.next;
-                  while (fast != null && fast.next != null) {
-                      slow = slow.next;
-                      fast = fast.next.next;
-                  }
-                  fast = null;
-                  ListNode curr = slow.next;
-                  while (curr != null) {
-                      ListNode temp1 = curr.next;
-                      curr.next = fast;
-                      fast = curr;
-                      curr = temp1;
-                  }
-                  slow.next = fast;
-                  fast = head;
-                  slow = slow.next;
-                  curr = slow;
-                  ListNode temp2 = fast.next;
-                  ListNode temp1 = slow.next;
-                  while (fast.next != curr) {
-                      temp2 = fast.next;
-                      temp1 = slow.next;
-                      fast.next = slow;
-                      slow.next = temp2;
-                      fast = temp2;
-                      slow = temp1;
-                  }
-                  fast.next = slow;
-                  // Your runtime beats 88.69 % of java submissions
-                  // Your memory usage beats 10.03 % of java submissions (48.7 MB)
-*/
+         * if (head.next == null || head.next.next == null)
+         * return;
+         * ListNode slow = head;
+         * ListNode fast = head.next.next;
+         * while (fast != null && fast.next != null) {
+         * slow = slow.next;
+         * fast = fast.next.next;
+         * }
+         * fast = null;
+         * ListNode curr = slow.next;
+         * while (curr != null) {
+         * ListNode temp1 = curr.next;
+         * curr.next = fast;
+         * fast = curr;
+         * curr = temp1;
+         * }
+         * slow.next = fast;
+         * fast = head;
+         * slow = slow.next;
+         * curr = slow;
+         * ListNode temp2 = fast.next;
+         * ListNode temp1 = slow.next;
+         * while (fast.next != curr) {
+         * temp2 = fast.next;
+         * temp1 = slow.next;
+         * fast.next = slow;
+         * slow.next = temp2;
+         * fast = temp2;
+         * slow = temp1;
+         * }
+         * fast.next = slow;
+         * // Your runtime beats 88.69 % of java submissions
+         * // Your memory usage beats 10.03 % of java submissions (48.7 MB)
+         */
     }
 
 }
